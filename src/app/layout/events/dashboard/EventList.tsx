5474ab8632382d0b997c0524cb0eea83726d4c97
mitkapanarin <mitka_j@yahoo.com>
2024-04-28 11:16:20 +0200

    event list items created

---
+++ src/app/layout/events/dashboard/EventList.tsx
@@ -0,0 +1,12 @@
+import { EventListItem } from "./EventListItem";
+
+export const EventList = () => {
+  return (
+    <>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </>
+  );
+};
---
+++ src/app/layout/events/dashboard/EventListAttendee.tsx
@@ -0,0 +1,9 @@
+import { Image, List } from "semantic-ui-react";
+
+export const EventListAttendee = () => {
+  return (
+    <List.Item>
+      <Image size="mini" circular src="/user.png" />
+    </List.Item>
+  );
+};

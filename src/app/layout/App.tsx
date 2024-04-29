8d9ff7606634d06071ded67f604a9bf2175d212e
Mitka Panarin <114948515+mitkapanarin@users.noreply.github.com>
2024-04-29 09:45:42 +0200

    added navlink

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,14 +1,15 @@
 import { Container } from "semantic-ui-react";
 import { NavBar } from "./nav/NavBar";
-
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "./home/HomePage";
 
 const App = () => {
+  const location = useLocation();
   return (
     <div>
       <NavBar />
       <Container className="main">
-        <Outlet />
+        {location.pathname === "/" ? <HomePage /> : <Outlet />}
       </Container>
     </div>
   );
--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,15 +1,18 @@
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
 
 export const NavBar = () => {
   return (
     <Menu inverted={true} fixed="top">
       <Container>
-        <MenuItem header>
+        <MenuItem header as={NavLink} to="/">
           <img src="/logo.png" alt="logo" />
         </MenuItem>
-        <MenuItem name="Events" />
+        <MenuItem name="Events" as={NavLink} to="/events" />
         <MenuItem>
           <Button
+            as={NavLink}
+            to="/createEvent"
             floated="right"
             positive={true}
             inverted={true}

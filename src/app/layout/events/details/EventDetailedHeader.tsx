93ec7d393d56878e7c7a808e342dff5d618e8900
Mitka Panarin <114948515+mitkapanarin@users.noreply.github.com>
2024-04-30 10:36:39 +0200

    .

--- src/app/layout/events/details/EventDetailedHeader.tsx
+++ src/app/layout/events/details/EventDetailedHeader.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Header, Image, Item, Segment } from "semantic-ui-react";
 
 const EventDetailedHeader = () => {
-  return <div>EventDetailedHeader</div>;
+  const eventImageStyle = {
+    filter: "brightness(30%)",
+  };
+
+  const eventImageTextStyle = {
+    position: "absolute",
+    bottom: "5%",
+    left: "5%",
+    width: "100%",
+    height: "auto",
+    color: "white",
+  };
+  return (
+    <Segment.Group>
+      <Segment basic attached="top" style={{ padding: "0" }}>
+        <Image src={`/drinks.jpg`} fluid style={eventImageStyle} />
+
+        <Segment basic style={eventImageTextStyle}>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Header
+                  size="huge"
+                  content="Event Title"
+                  style={{ color: "white" }}
+                />
+                <p>Event Date</p>
+                <p>
+                  Hosted by <strong>Bob</strong>
+                </p>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+      </Segment>
+    </Segment.Group>
+  );
 };
 
 export default EventDetailedHeader;
